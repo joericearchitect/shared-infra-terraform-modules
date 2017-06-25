e2cb618bafcaf0de162ec9bcb7b2ba098686c001
Joe Rice <joericearchitect@gmail.com>
2017-06-25 15:06:12 -0400

    small incremental changes to terraform modules to test

--- modules/aws/resources/aws/vars.tf
+++ modules/aws/resources/aws/vars.tf
@@ -1,8 +1,4 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "aws_key_path" {}
-variable "aws_key_name" {}
 
-variable "region" {
-  default = "us-east-1"
-}
+variable "region" {}
--- test-environments/fixed-env-small/main.tf
+++ test-environments/fixed-env-small/main.tf
@@ -2,6 +2,8 @@ module "aws" {
   source = "github.com/joericearchitect/shared-infra-terraform-modules//modules/aws/resources/aws"
   
   region = "${var.region}"
+  aws_access_key = "${var.aws_access_key}"
+  aws_secret_key = "${var.aws_secret_key}"
 }
 
 module "vpc" {
