e2121f989aacfeb289dd8670051c31ab9d7a50f0
Joe Rice <joericearchitect@gmail.com>
2017-06-27 13:34:10 -0400

    small incremental changes to terraform modules to test

--- modules/aws/environment/region/vpc/availability-zone/private-subnet/main.tf
+++ modules/aws/environment/region/vpc/availability-zone/private-subnet/main.tf
@@ -2,16 +2,12 @@ module "private-subnet"  {
   source = "github.com/joericearchitect/shared-infra-terraform-modules//modules/aws/resources/subnet"
   
   region = "${var.region}"
-  region-name = "${var.region-name}"
   
   availability-zone-id = "${var.availability-zone-id}"
-  availability-zone-name = "${var.availability-zone-name}" 
   
-  region-ami-lookup = "${var.region-ami-lookup}"
-  
-  aws-vpc-id = "${module.vpc.aws-vpc-id}"
-  aws-internet-gateway-id = "${module.internet-gateway.aws-internet-gateway-id}"
-  aws-route-table-id = "${module.private-route-table.aws-route-table-id}"
+  aws-vpc-id = "${var.aws-vpc-id}"
+  aws-internet-gateway-id = "${var.aws-internet-gateway-id}"
+  aws-route-table-id = "${var.aws-route-table-id}"
   jra-subnet-type = "private"
  
   vpc-cidr-1 = "${var.vpc-cidr-1}"
@@ -21,10 +17,10 @@ module "private-subnet"  {
   subnet-cidr-4 = "${var.subnet-cidr-4}"
   subnet-cidr-range = "${var.subnet-cidr-range}"
   
-  subnet-config-public-ip-on-launch = "true"
+  subnet-config-public-ip-on-launch = 	"true"
   
   environment-group= "${var.environment-group}"
-  environment-instance-id = "${random_id.env-instance.b64}"
+  environment-instance-id = "${environment-instance-id}"
   environment-canonical-id = "${var.environment-canonical-id}"
   environment-display-id = "${var.environment-display-id}"
   environment-name = "${var.environment-name}"
