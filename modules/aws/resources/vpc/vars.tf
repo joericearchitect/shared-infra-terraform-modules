b3d57d36e189ae61f8b569b38919fd3711b0c5d1
Joe Rice <joericearchitect@gmail.com>
2017-06-26 22:59:52 -0400

    small incremental changes to terraform modules to test

--- modules/aws/resources/vpc/vars.tf
+++ modules/aws/resources/vpc/vars.tf
@@ -26,6 +26,8 @@ variable "vpc-cidr-3" {}
 variable "vpc-cidr-4" {}
 variable "vpc-cidr-range" {}
 
-variable "vpc-cidr" {}
+variable "vpc-cidr" {
+  default = ""
+}
 
 vpc-cidr = "${vpc-cidr-1}.${vpc-cidr-2}.${vpc-cidr-3}.${vpc-cidr-4}/${vpc-cidr-range}"
